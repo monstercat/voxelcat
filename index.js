40186934ae711a932b71864e956ae59a9a644997
Bill Casarin <bill@casarin.ca>
2013-05-24 12:36:03 -0400

    Initial

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var player = require('voxel-player')
 var texturePath = require('painterly-textures')(__dirname)
 var voxel = require('voxel')
 var extend = require('extend')
+var toolbar = require('toolbar')
+var bartab = toolbar('.bar-tab')
 
 module.exports = function(opts, setup) {
   setup = setup || defaultSetup
   var defaults = {
-    generate: voxel.generator['Valley'],
+    generate: voxel.generator.Valley,
     chunkDistance: 2,
     materials: [
       ['grass', 'dirt', 'grass_dirt'],
@@ -25,18 +27,23 @@ module.exports = function(opts, setup) {
 
   // setup the game and add some trees
   var game = createGame(opts)
+  game.gravity = [0, -0.000009, 0];
   var container = opts.container || document.body
   window.game = game // for debugging
   game.appendTo(container)
   if (game.notCapable()) return game
   
-  var createPlayer = player(game)
+  var createPlayer = player(game);
 
   // create the player from a minecraft skin file and tell the
   // game to use it as the main player
   var avatar = createPlayer(opts.playerSkin || 'player.png')
   avatar.possess()
   avatar.yaw.position.set(2, 14, 4)
+
+  game.avatar = avatar;
+
+  console.log(avatar);
 
   setup(game, avatar)
   
@@ -48,6 +55,7 @@ function defaultSetup(game, avatar) {
   // highlight blocks when you look at them, hold <Ctrl> for block placement
   var blockPosPlace, blockPosErase
   var hl = game.highlighter = highlight(game, { color: 0xff0000 })
+  var mat = 3;
   hl.on('highlight', function (voxelPos) { blockPosErase = voxelPos })
   hl.on('remove', function (voxelPos) { blockPosErase = null })
   hl.on('highlight-adjacent', function (voxelPos) { blockPosPlace = voxelPos })
@@ -58,18 +66,22 @@ function defaultSetup(game, avatar) {
     if (ev.keyCode === 'R'.charCodeAt(0)) avatar.toggle()
   })
 
+  bartab.on("select", function(item){
+    switch(item) {
+      case 'Crate': mat = 5; break;
+      case 'Brick': mat = 3; break;
+      default: break;
+    }
+  });
+
   // block interaction stuff, uses highlight data
   var currentMaterial = 1
 
   game.on('fire', function (target, state) {
     var position = blockPosPlace
-    if (position) {
-      game.createBlock(position, currentMaterial)
-    }
-    else {
-      position = blockPosErase
-      if (position) game.setBlock(position, 0)
-    }
+
+    var results = game.raycastVoxels()
+    game.createAdjacent(results, mat)
   })
 
 }
